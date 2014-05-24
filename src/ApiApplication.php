42f65dd4fce4ec6bb47ad2436ab709d13c71446f
gossi <github@gos.si>
2014-05-24 12:32:53 +0200

    bare app

---
+++ src/ApiApplication.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace keeko\application\api;
+
+use keeko\core\application\AbstractApplication;
+use Symfony\Component\HttpFoundation\Request;
+
+class ApiApplication extends AbstractApplication {
+	
+	
+	/* (non-PHPdoc)
+	 * @see \keeko\core\application\AbstractApplication::run()
+	 */
+	public function run(Request $request) {
+
+	}
+
+}
